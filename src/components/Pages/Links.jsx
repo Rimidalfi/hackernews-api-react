b80ef9390c36d475f7450e8d5238b082e42187dd
DESKTOP-AAHU0IQ\Tilo <info@tilorenner.com>
2023-10-24 18:16:30 +0200

    add UI Elements, add State for CurrentMainComponent

---
+++ src/components/Pages/Links.jsx
@@ -0,0 +1,12 @@
+export default function Links() {
+    const myStyle = {
+      border: "2px solid green",
+      backgroundColor: "orange",
+    };
+    return (
+      <div style={myStyle}>
+        <h1>Links</h1>
+        <p>Links comming soon</p>
+      </div>
+    );
+  }
---
+++ src/components/Pages/NewsGuidelines.jsx
@@ -0,0 +1,12 @@
+export default function NewsGuidelines() {
+    const myStyle = {
+      border: "2px solid green",
+      backgroundColor: "orange",
+    };
+    return (
+      <div style={myStyle}>
+        <h1>Guidelines</h1>
+        <p>Guidelines comming soon</p>
+      </div>
+    );
+  }

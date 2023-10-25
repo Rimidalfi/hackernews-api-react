c5261a0922afd49b40f028ec3472923b22280527
Rick <rlochner@gmail.com>
2023-10-25 08:58:58 +0200

    changes styling

--- src/components/Pages/Homepage.jsx
+++ src/components/Pages/Homepage.jsx
@@ -3,19 +3,15 @@ import "./Pages.css"
 export default function Homepage() {
   return (
     <div>
-     <table border="0" cellPadding="0" cellSpacing="0" >
+      
+     {/* <table border="0" cellPadding="0" cellSpacing="0" >
       <tbody>
       <tr className='athing' id='38002752'>
       <td align="right" valign="top" className="title"><span className="rank">1.</span></td>      <td valign="top" className="votelinks"><center><a id='up_38002752'href='vote?id=38002752&amp;how=up&amp;goto=news'><div className='votearrow' title='upvote'></div></a></center></td><td className="title"><span className="titleline"><a href="https://www.reuters.com/business/autos-transportation/california-suspends-gm-cruises-driverless-autonomous-vehicle-permits-2023-10-24/" rel="noreferrer">California suspends Cruise&#x27;s autonomous vehicle deployment</a><span className="sitebit comhead"> (<a href="from?site=reuters.com"><span className="sitestr">reuters.com</span></a>)</span></span></td></tr><tr><td colspan="2"></td><td className="subtext"><span className="subline">
           <span className="score" id="score_38002752">696 points</span> by <a href="user?id=ra7" className="hnuser">ra7</a> <span className="age" title="2023-10-24T17:55:33"><a href="item?id=38002752">9 hours ago</a></span> <span id="unv_38002752"></span> | <a href="hide?id=38002752&amp;goto=news">hide</a> | <a href="item?id=38002752">867&nbsp;comments</a>        </span>
               </td></tr>
-              {/* <tr className="spacer" style="height:5px"></tr>
-                <tr className='athing' id='37999035'>
-      <td align="right" valign="top" className="title"><span className="rank">2.</span></td>      <td valign="top" className="votelinks"><center><a id='up_37999035'href='vote?id=37999035&amp;how=up&amp;goto=news'><div className='votearrow' title='upvote'></div></a></center></td><td className="title"><span className="titleline"><a href="https://animatedai.github.io/" rel="noreferrer">Animated AI</a><span className="sitebit comhead"> (<a href="from?site=animatedai.github.io"><span className="sitestr">animatedai.github.io</span></a>)</span></span></td></tr><tr><td colspan="2"></td><td className="subtext"><span className="subline">
-          <span className="score" id="score_37999035">543 points</span> by <a href="user?id=qwertyforce" className="hnuser">qwertyforce</a> <span className="age" title="2023-10-24T13:39:54"><a href="item?id=37999035">13 hours ago</a></span> <span id="unv_37999035"></span> | <a href="hide?id=37999035&amp;goto=news">hide</a> | <a href="item?id=37999035">51&nbsp;comments</a>        </span>
-              </td></tr> */}
       </tbody>
-     </table>
+     </table> */}
     </div>
   );
 }

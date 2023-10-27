3dcf6a3974231054bdb948ea2c492938577198e1
Wowa <rimidalf@tutamail.com>
2023-10-27 16:36:14 +0200

    comment structure initialized

--- src/components/Layouts/MainLayout.jsx
+++ src/components/Layouts/MainLayout.jsx
@@ -1,10 +1,12 @@
 import Header from "../Header";
 import Footer from "../Footer";
+import Comments from "../Pages/API/Comments";
 export default function MainLayout({ children, setMainComponent }) {
   return (
     <>
       <Header setMainComponent={setMainComponent} />
-      <main className="mainContainer">{children}</main>
+      <Comments className="mainContainer" />
+      {/* <main className="mainContainer">{children}</main> */}
       <hr className="horizontalLine" />
       <Footer setMainComponent={setMainComponent} />
     </>

d753e7f91ef90aadeb88a41629540af6048b0390
DESKTOP-AAHU0IQ\Tilo <info@tilorenner.com>
2023-10-27 14:36:04 +0200

    display adjustments for stories

--- src/components/Pages/API/Story.jsx
+++ src/components/Pages/API/Story.jsx
@@ -1,40 +1,78 @@
 import CommentsForArticle from "./CommentsForArticle";
+import UserPage from "./UserPage";
 import React from "react";
 import { useContext } from "react";
-//import setMainCompContext from "../../../App";
-//import setMainComponent from "../../Utils/setMainComponent";
+import setMainCompContext from "../../../App";
+
 
 export default function Story({item,setMainComponent})
 {
 
+    console.log("Title",item.title)
     //const setMainComponent2 = useContext(setMainCompContext)
+
+
+    //const created_at_MS = Date.parse(item.created_at);
+
+     
+
+    //console.log("URL:", item.url);
+    //const url = new URL(item.url);
+    //const baseUrl = '{url.protocol}//${{url.hostname}'
+    //const baseUrl="fake";
+
+    const baseURL = extractBaseURL(item.url)
+
+    function extractBaseURL(url)
+    {
+        if(url)
+        {
+            const url = new URL(item.url);
+            const baseURL = '{url.protocol}//${{url.hostname}'
+            return baseURL;
+        }
+
+        return null;
+
+    }
+
+
+    function calculateTimePassed(timeSource)
+    {
+        const passedMSDate = new Date()
+        passedMSDate.setTime(Date.now() - Date.parse(timeSource))
+    
+        return passedMSDate.getHours();
+    }
 
     function switchToComments()
     {
-       // const component = React.cloneElement(<CommentsForArticle/>,{setMainComponent:setMainComponent}); //Add prop
-        //const component = React.cloneElement(<CommentsForArticle/>,{item:item,setMainComponent:setMainComponent}) //Add prop
         const component = <CommentsForArticle item={item} setMainComponent={setMainComponent}/>
+        setMainComponent(component);
+    }
+    function switchToAuthor()
+    {
+        const component = <UserPage item={item} setMainComponent={setMainComponent}/>
         setMainComponent(component);
     }
 
 
     return(
         <>
-            <span>Story     
-            </span>
-            <div>
-            </div>
-            <div>Author {item.author}</div>
-            <div>ID {item.story_id}</div>
-            <div>Created {item.created_at}</div>
-            <div>StoryText {item.story_text}</div>
-            <div>Titel {item.title}</div>
-            <div>Children: {item.children ? item.children.length : 0}</div>
-            <div onClick={()=> switchToComments()}>Comments:{item.num_comments}</div>
-            <div>URL:{item.url}</div>
-            <div>Updated: {item.updated_at}</div>
-            <div>Points: {item.points}</div>
-            <br></br>
+
+            <a href={item.url}>Titel {item.title} </a> 
+            {
+                baseURL? <a href={baseURL}>({item.url}) </a> : ""
+            }
+
+            <br/>
+            <span>{item.points} Points by </span>
+            <a href="#" onClick={()=> switchToAuthor()}>{item.author} </a>
+            <a href="#" onClick={()=> switchToComments()}>{calculateTimePassed(item.created_at)} | hide | </a>
+
+            <a name="comments" href="#" onClick={()=> switchToComments()}>{item.num_comments ? item.num_comments : 'discuss'} comments</a>
+            <br/><br/>
+            {/*<div>Children: {item.children ? item.children.length : 0}</div>*/}
         </>
 
     );

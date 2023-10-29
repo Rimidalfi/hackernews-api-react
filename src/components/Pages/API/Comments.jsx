1c9d2cfddc8c1b4847f2a731adaf6640be968e4f
Wowa <rimidalf@tutamail.com>
2023-10-29 20:06:27 +0100

    embedding comment API Fetch

--- src/components/Pages/API/Comments.jsx
+++ src/components/Pages/API/Comments.jsx
@@ -1,11 +1,7 @@
 import { useState, useEffect } from "react";
+import CommentsStructure from "./CommentStructure";
 
 export default function Comments({ setMainComponent }) {
-  const placeholderComm =
-    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi provident perferendis veniam unde vitae aliquam earum sint. Suscipit molestias provident architecto, corrupti itaque et sint deserunt aut vitae, quaerat sit?";
-  const placeholderLink = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-  const placeholderAuthor = "Jeremiah";
-  const placeholderStory = "Tikun Olam";
   const [comments, setComments] = useState();
   const URL =
     "https://hn.algolia.com/api/v1/search_by_date?tags=comment&numericFilters=created_at_i%3E5400&hitsPerPage=30";
@@ -13,50 +9,22 @@ export default function Comments({ setMainComponent }) {
     fetch(URL)
       .then((response) => response.json())
       .then((data) => {
-        setComments(data);
+        setComments(data.hits);
         console.log("API FETCH COMMENT:", comments);
       });
   }, []);
+  const myComments = comments?.map((comment) => {
+    return (
+      <CommentsStructure
+        author={comment.author}
+        age={comment.created_at}
+        parent={comment.story_title}
+        commentText={comment.comment_text}
+        link={comment.story_url !== undefined ? comment.story_url : "NO LINK"}
+        key={Number(comment.objectID)}
+      />
+    );
+  });
 
-  return (
-    <>
-      <td>
-        <a href="" className="voteArrow">
-          <img src="" alt="" />
-        </a>
-      </td>
-      <td>
-        <div className="commHead">
-          <a href="" className="commAuthor">
-            {placeholderAuthor}
-          </a>
-          <span>
-            <a href="" className="commAge">
-              {placeholderStory}
-            </a>
-            <span>
-              <a href="" className="commParent">
-                parent
-              </a>
-              <a href="" className="commContext">
-                context
-              </a>
-              <a href="" className="commNext">
-                next
-              </a>
-              <br />
-              <div>
-                <span>
-                  {placeholderComm}
-                  <p>
-                    <a href="">{placeholderLink}</a>
-                  </p>
-                </span>
-              </div>
-            </span>
-          </span>
-        </div>
-      </td>
-    </>
-  );
+  return <>{myComments}</>;
 }
